e9583aaf6801095f26302921597ca3282405158c
stormzhang <zq329051@outlook.com>
2018-07-24 10:09:23 +0800

    集成 hotfix 更新资源

--- platform_www/cordova_plugins.js
+++ platform_www/cordova_plugins.js
@@ -7,13 +7,22 @@ module.exports = [
     "clobbers": [
       "navigator.setting"
     ]
+  },
+  {
+    "id": "cordova-hot-code-push-plugin.chcp",
+    "file": "plugins/cordova-hot-code-push-plugin/www/chcp.js",
+    "pluginId": "cordova-hot-code-push-plugin",
+    "clobbers": [
+      "chcp"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-setting": "1.0.0"
+  "cordova-plugin-setting": "1.0.0",
+  "cordova-hot-code-push-plugin": "1.5.3"
 };
 // BOTTOM OF METADATA
 });
